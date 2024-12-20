7ec0a50ff7ea91e0761d021e940b7e4b62ef74bb
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-12-20 13:23:10 +0200

    new

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use((req, res, next) => {
     res.header(
         "Access-Control-Allow-Origin",
-        "https://habyarimanacaleb.github.io"
+        "https://kivu-grafter.netlify.app/"
     );
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
