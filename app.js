d715f8c51bd9b4c65153acaf07c7c15ce80c1ef8
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-12-20 11:39:53 +0200

    New commits

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const serviceroutes = require("./routes/reviewRoutes")
 app.use(express.json());
 app.use(express.static("public"));
 
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    next();
+});
 app.get("/", (req, res) => {
 
 });
